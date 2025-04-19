4244f9e4e878b48896c369db24586a91ebf7be81
Johnson Alala <jonesalala95@gmail.com>
2025-04-19 23:26:21 +0300

    Future Girlfriends

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -20,10 +20,29 @@ function ShoppingList({ items }) {
           value={selectedCategory}
           onChange={handleCategoryChange}
         >
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
+          <option value="All">Filter by personality trait</option>
+          <option value="Courageous Explorer">Courageous Explorer</option>
+          <option value="Visionary Artist">Visionary Artist</option>
+          <option value="Laughter Magnet">Laughter Magnet</option>
+          <option value="Intellectual Leader">Intellectual Leader</option>
+          <option value="Fearless Adventurer">Fearless Adventurer</option>
+          <option value="Creative Genius">Creative Genius</option>
+          <option value="Charming Storyteller">Charming Storyteller</option>
+          <option value="Innovative Thinker">Innovative Thinker</option>
+          <option value="Joyful Optimist">Joyful Optimist</option>
+          <option value="Dream Chaser">Dream Chaser</option>
+          <option value="Masterful Problem Solver">
+            Masterful Problem Solver
+          </option>
+          <option value="Endless Jokester">Endless Jokester</option>
+          <option value="Deep Thinker">Deep Thinker</option>
+          <option value="Adventurous Spirit">Adventurous Spirit</option>
+          <option value="Empathic Listener">Empathic Listener</option>
+          <option value="World Traveler">World Traveler</option>
+          <option value="Witty Entertainer">Witty Entertainer</option>
+          <option value="Grounded Philosopher">Grounded Philosopher</option>
+          <option value="Fearless Leader">Fearless Leader</option>
+          <option value="Creative Visionary">Creative Visionary</option>
         </select>
       </div>
       <ul className="Items">
--- src/data/items.js
+++ src/data/items.js
@@ -1,9 +1,24 @@
-const items = [
-  { id: 1, name: "Yogurt", category: "Dairy" },
-  { id: 2, name: "Pomegranate", category: "Produce" },
-  { id: 3, name: "Lettuce", category: "Produce" },
-  { id: 4, name: "String Cheese", category: "Dairy" },
-  { id: 5, name: "Cookies", category: "Dessert" },
+const itemData = [
+  { id: 1, name: "Amina Noor", category: "Courageous Explorer" },
+  { id: 2, name: "Martha Wambui", category: "Visionary Artist" },
+  { id: 3, name: "Sophie Nyambura", category: "Laughter Magnet" },
+  { id: 4, name: "Grace Njeri", category: "Intellectual Leader" },
+  { id: 5, name: "Zawadi Achieng", category: "Fearless Adventurer" },
+  { id: 6, name: "Lucy Wanjiku", category: "Creative Genius" },
+  { id: 7, name: "Nadia Muthoni", category: "Charming Storyteller" },
+  { id: 8, name: "Joyce Anyango", category: "Innovative Thinker" },
+  { id: 9, name: "Faith Mutheu", category: "Joyful Optimist" },
+  { id: 10, name: "Mercy Chebet", category: "Dream Chaser" },
+  { id: 11, name: "Daniel Mwangi", category: "Masterful Problem Solver" },
+  { id: 12, name: "Peter Otieno", category: "Endless Jokester" },
+  { id: 13, name: "James Kamau", category: "Deep Thinker" },
+  { id: 14, name: "Joseph Mwangi", category: "Adventurous Spirit" },
+  { id: 15, name: "Kevin Njoroge", category: "Empathic Listener" },
+  { id: 16, name: "David Kiprotich", category: "World Traveler" },
+  { id: 17, name: "John Wambua", category: "Witty Entertainer" },
+  { id: 18, name: "Dennis Ndungu", category: "Grounded Philosopher" },
+  { id: 19, name: "Brian Mutisya", category: "Fearless Leader" },
+  { id: 20, name: "Michael Kipketer", category: "Creative Visionary" },
 ];
 
-export default items;
+export default itemData;
